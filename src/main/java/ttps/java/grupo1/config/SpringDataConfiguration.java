d6c22689ff20dc68084e235aa919667b713fd08a
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-11-07 20:27:29 -0300

    Singleton

--- src/main/java/ttps/java/grupo1/config/SpringDataConfiguration.java
+++ src/main/java/ttps/java/grupo1/config/SpringDataConfiguration.java
@@ -2,14 +2,18 @@ package ttps.java.grupo1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import ttps.java.grupo1.service.UserService;
+
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_SINGLETON;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "ttps.java.grupo1.DAO")
 public class SpringDataConfiguration {
 
     @Bean
+    @Scope(scopeName = SCOPE_SINGLETON)
     public UserService springDataJpaService() {
         return new UserService();
     }
