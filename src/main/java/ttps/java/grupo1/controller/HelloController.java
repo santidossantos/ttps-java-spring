569c1193546ba2177d6423ec232b5b674f6da9b9
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-11-07 19:05:28 -0300

    Update

--- src/main/java/ttps/java/grupo1/controller/HelloController.java
+++ src/main/java/ttps/java/grupo1/controller/HelloController.java
@@ -1,6 +1,5 @@
 package ttps.java.grupo1.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
