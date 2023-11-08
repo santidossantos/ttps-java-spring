8982fac01cf0567e06654de5637ad32712854065
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-11-08 12:03:52 -0300

    Agrego anotaciones @transactional en el service

--- src/main/java/ttps/java/grupo1/service/UserService.java
+++ src/main/java/ttps/java/grupo1/service/UserService.java
@@ -1,6 +1,7 @@
 package ttps.java.grupo1.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import ttps.java.grupo1.DAO.UserDAO;
 import ttps.java.grupo1.model.User;
 
@@ -9,13 +10,11 @@ public class UserService {
     @Autowired
     UserDAO userDAO;
 
-    public UserService() {
-
-    }
-
+    @Transactional(readOnly = true)
     public String findByEmail() {
         return this.userDAO.findByEmail("jorge11@gmail.com").getEmail();
     }
 
+    @Transactional
     public User saveUser(User user){ return this.userDAO.save(user); }
 }
