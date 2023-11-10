def1b8b302370f1357a879472336f569ec14a819
Sun <santiago.lizondo@gmail.com>
2023-11-10 12:10:21 -0300

    con mockmvc

---
+++ src/test/java/ttps/java/grupo1/controller/UserControllerTest.java
@@ -0,0 +1,39 @@
+package ttps.java.grupo1.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ttps.java.grupo1.model.User;
+import ttps.java.grupo1.service.UserService;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    void createUser() throws Exception {
+        User user = new User("Laura", "john.doe", "password123", "john@example.com");
+        String userJson = objectMapper.writeValueAsString(user);
+        mockMvc.perform(MockMvcRequestBuilders.post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userJson))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+}
+
+
+
