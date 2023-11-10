353940c4fbe80b38b7c80108d63a886e57912754
Sun <santiago.lizondo@gmail.com>
2023-11-10 12:25:46 -0300

    primer test ready

--- src/test/java/ttps/java/grupo1/controller/UserControllerTest.java
+++ src/test/java/ttps/java/grupo1/controller/UserControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ttps.java.grupo1.controller.UserController;
 import ttps.java.grupo1.model.User;
 import ttps.java.grupo1.service.UserService;
 
@@ -34,6 +35,7 @@ class UserControllerTest {
                         .andExpect(MockMvcResultMatchers.status().isCreated());
     }
 }
+
 
 
 
