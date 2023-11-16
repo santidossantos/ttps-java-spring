3de7c01c6e83b7ecd2377e7baea11c89cdb30539
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-11-16 17:22:09 -0300

    REST CRUD working

--- src/main/java/ttps/java/grupo1/controller/UserController.java
+++ src/main/java/ttps/java/grupo1/controller/UserController.java
@@ -2,7 +2,6 @@ package ttps.java.grupo1.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +11,7 @@ import ttps.java.grupo1.service.UserService;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Validated
@@ -30,20 +30,33 @@ public class UserController {
                 : new ResponseEntity<>(userList, HttpStatus.OK);
     }
 
-    @PostMapping
-    public ResponseEntity<User> create(@Valid @RequestBody User user) {
-        try {
-            return new ResponseEntity<User>(userService.save(user), HttpStatus.CREATED);
-        }
-        catch (DataIntegrityViolationException e) {
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
-        }
-        catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> get(@PathVariable("id") Long id) {
+        Optional<User> user = this.userService.findById(id);
+        return user.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).
+                orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @PostMapping
+    public ResponseEntity<User> create(@Valid @RequestBody User user) {
+        return new ResponseEntity<>(this.userService.save(user), HttpStatus.CREATED);
+    }
 
+    @PutMapping
+    public ResponseEntity<User> update(@Valid @RequestBody User user) {
+        return new ResponseEntity<>(this.userService.save(user), HttpStatus.OK);
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> delete(@PathVariable("id") Long id) {
+        Optional<User> user = this.userService.findById(id);
+        if(user.isPresent()) {
+            this.userService.deleteById(id);
+        }
+        return user.isPresent()
+                ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
 
 }
--- src/main/java/ttps/java/grupo1/service/UserService.java
+++ src/main/java/ttps/java/grupo1/service/UserService.java
@@ -1,14 +1,13 @@
 package ttps.java.grupo1.service;
 
-import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ttps.java.grupo1.repository.UserRepository;
 import ttps.java.grupo1.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -22,7 +21,17 @@ public class UserService {
     }
 
     @Transactional
-    public User save(User user) throws DataAccessException {
+    public User save(User user) {
         return this.userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<User> findById(Long id) {
+        return this.userRepository.findById(id);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        this.userRepository.deleteById(id);
     }
 }
