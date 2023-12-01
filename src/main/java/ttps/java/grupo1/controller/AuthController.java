70fda82ed8516715eb48dfd74c7a3c67a33e0244
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-12-01 08:14:13 -0300

    JWT Pruebas

--- src/main/java/ttps/java/grupo1/controller/AuthController.java
+++ src/main/java/ttps/java/grupo1/controller/AuthController.java
@@ -1,47 +1,24 @@
 package ttps.java.grupo1.controller;
 
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ttps.java.grupo1.DTO.AuthResponseDTO;
 import ttps.java.grupo1.DTO.LoginDTO;
-import ttps.java.grupo1.DTO.RegisterDTO;
-import ttps.java.grupo1.exception.DuplicateConstraintException;
-import ttps.java.grupo1.model.User;
-import ttps.java.grupo1.service.UserService;
+import ttps.java.grupo1.security.JwtService;
 
 @RestController
-@RequestMapping(value = "/auth", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AuthController {
 
     @Autowired
-    private UserService userService;
+    private JwtService jwtService;
 
-//    @PostMapping("/register")
-//    public ResponseEntity<Object> register(@Valid @RequestBody RegisterDTO registerDTO) {
-//        User user = new User(registerDTO.getName(), registerDTO.getUsername(),
-//                registerDTO.getPassword(), registerDTO.getEmail()
-//        );
-//
-//        try {
-//            user = userService.register(user);
-//        }
-//        catch (DuplicateConstraintException e) {
-//            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
-//        }
-//        return new ResponseEntity<>(user, HttpStatus.CREATED);
-//    }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<AuthResponseDTO> login(@Valid @RequestBody LoginDTO loginDTO) {
-//        String token = userService.authenticate(loginDTO.getUsername(), loginDTO.getPassword());
-//        return new ResponseEntity<>(new AuthResponseDTO(token), HttpStatus.OK);
-//    }
-
+    @PostMapping("/login")
+    public String login(@RequestBody LoginDTO loginRequest) {
+        // Aquí deberías autenticar al usuario y verificar las credenciales
+        // Si las credenciales son válidas, genera y devuelve el token
+        String username = loginRequest.getUsername();
+        String token = jwtService.generateToken(username);
+        return token;
+    }
 }
