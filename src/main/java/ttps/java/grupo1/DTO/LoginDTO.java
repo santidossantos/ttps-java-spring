5cd126a45f2bb40d4c40ac561bf058e396c7dd94
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-12-05 16:31:02 -0300

    Fix cors

--- src/main/java/ttps/java/grupo1/DTO/LoginDTO.java
+++ src/main/java/ttps/java/grupo1/DTO/LoginDTO.java
@@ -3,9 +3,11 @@ package ttps.java.grupo1.DTO;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginDTO {
 
     @NotEmpty
