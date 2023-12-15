7a3e7d69b2fa92ede19565e236883c9e506eed09
Sun <santiago.lizondo@gmail.com>
2023-12-15 14:36:42 -0300

    Endpoint/get all categories (#20)
    * fix valid
    
    * get all categories

--- src/main/java/ttps/java/grupo1/service/GroupCategoryService.java
+++ src/main/java/ttps/java/grupo1/service/GroupCategoryService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import ttps.java.grupo1.model.GroupCategory;
 import ttps.java.grupo1.repository.GroupCategoryRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,9 +20,13 @@ public class GroupCategoryService {
         return this.groupCategoryRepository.save(groupCategory);
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     public Optional<GroupCategory> findById(Long id) {
         return this.groupCategoryRepository.findById(id);
     }
 
+    @Transactional(readOnly = true)
+    public List<GroupCategory> findAll() {
+        return this.groupCategoryRepository.findAll();
+    }
 }
