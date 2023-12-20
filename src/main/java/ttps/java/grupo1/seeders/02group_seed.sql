0e912b87804ddf637a03b449a794a0f71b941126
Sun <santiago.lizondo@gmail.com>
2023-12-20 16:00:06 -0300

    imagenes iconos

--- src/main/java/ttps/java/grupo1/seeders/02group_seed.sql
+++ src/main/java/ttps/java/grupo1/seeders/02group_seed.sql
@@ -2,19 +2,19 @@ SET FOREIGN_KEY_CHECKS=0;
 DELETE FROM `group`;
 ALTER TABLE `group` AUTO_INCREMENT = 0;
 
-INSERT INTO `group` (`name`, `hidden`, `category_id`)
-VALUES ('Viaje a brasil', false, 2);
+INSERT INTO `group` (`name`, `hidden`, `category_id`, `img`)
+VALUES ('Viaje a brasil', false, 2, 'local_airport');
 
-INSERT INTO `group` (`name`, `hidden`, `category_id`)
-VALUES ('Viaje a china', false, 2);
+INSERT INTO `group` (`name`, `hidden`, `category_id`, `img`)
+VALUES ('Viaje a china', false, 2, 'local_airport');
 
-INSERT INTO `group` (`name`, `hidden`, `category_id`)
-VALUES ('Viaje a japon', false, 2);
+INSERT INTO `group` (`name`, `hidden`, `category_id`, `img`)
+VALUES ('Viaje a japon', false, 2, 'local_airport');
 
-INSERT INTO `group` (`name`, `hidden`, `category_id`)
-VALUES ('Cena en pasillo', false, 3);
+INSERT INTO `group` (`name`, `hidden`, `category_id`, `img`)
+VALUES ('Cena en pasillo', false, 3, 'local_bar');
 
-INSERT INTO `group` (`name`, `hidden`, `category_id`)
-VALUES ('Supermercado vea', false, 1);
+INSERT INTO `group` (`name`, `hidden`, `category_id`, `img`)
+VALUES ('Supermercado vea', false, 1, 'group');
 
 SET FOREIGN_KEY_CHECKS=1;
